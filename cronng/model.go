6e1fd53b56b8f71abfd901a48a09497831c38386
Kenichi Sasaki <siroken3@gmail.com>
2015-09-09 02:15:52 +0900

    defined data models (continuing.)

--- cronng/model.go
+++ cronng/model.go
@@ -1,30 +1,87 @@
 package cronng
 
-type JobDefinition struct {
-	Name       string `json:"name"`
-	Definition string `json:"definition"`
-}
-
-type JobId string
-
 type Job struct {
-	Id         JobId
-	Name       string
-	Definition string
-	CreatedAt  time
+	uuid               UUID
+	name               string
+	description        string
+	group              string
+	multipleExecutions bool
+	timeout            Timeout
+	sequence           []Command
+	dispatch           Dispatch
+	schedule           Schedule
+	notification       Notification
 }
 
-type ProcId string
+type Timtout string
 
-type Proc struct {
-	Id          ProcId
-	JobId       JobId
-	TriggerType string
-	Crontab     string
-	StartAt     time
+type Command struct {
+	exec   string
+	jobref Job
+	script Script
 }
 
-func NewJob(def JobDefinition) (job *Job) {
+type Dispatch struct {
+	threadcount int
+	keepgoing   bool
+}
+
+type Crontab string
+
+type Schedule struct {
+	crontab Crontab
+}
+
+type Notification struct {
+	onsuccess string
+	onfailure string
+	onstart   string
+}
+
+// Execution
+
+type Execution struct {
+	uuid            UUID
+	output          URL
+	status          Status
+	user            User
+	started         Time
+	description     string
+	job             Job
+	args            string
+	statistics      Statistics
+	ended           Time
+	abortedBy       User
+	successfulNodes []Node
+	failedNodes     []Node
+}
+
+const (
+	RUNNING = iota
+	SUCCEEDED
+	FAILED
+	ABORTED
+)
+
+type URL struct {
+}
+
+type User struct {
+}
+
+type Statistics struct {
+}
+
+type Time stuct {
+}
+
+type Node struct {
+}
+
+//
+// coding snippets are below
+//
+func NewJob(script Script) (job *Job) {
 	// Create Job instance
 	job := &Job{}
 	// Persist Job instance
