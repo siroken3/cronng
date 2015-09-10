01e33d5cc654613379228679b527a79abac6ee6c
Kenichi Sasaki <siroken3@gmail.com>
2015-09-11 01:13:37 +0900

    correct model struct

--- cronng/model.go
+++ cronng/model.go
@@ -1,61 +1,30 @@
 package cronng
 
+import "code.google.com/p/go-uuid/uuid"
+import "net/url"
+import "time"
+
 type Job struct {
-	uuid               UUID
+	uuid               uuid.UUID
 	name               string
 	description        string
 	group              string
 	multipleExecutions bool
-	timeout            Timeout
-	sequence           []Command
-	dispatch           Dispatch
+	timeout            time.Time
+	script             Script
 	schedule           Schedule
 	notification       Notification
 }
 
-type Timtout string
-
-type Command struct {
-	exec   string
-	jobref Job
-	script Script
-}
-
-type Dispatch struct {
-	threadcount int
-	keepgoing   bool
-}
-
-type Crontab string
-
-type Schedule struct {
-	crontab Crontab
-}
+type Schedule string
 
 type Notification struct {
-	onsuccess string
-	onfailure string
-	onstart   string
+	onstart   [](func(Execution) int)
+	onsuccess [](func(Execution) int)
+	onfailure [](func(Execution) int)
 }
 
 // Execution
-
-type Execution struct {
-	uuid            UUID
-	output          URL
-	status          Status
-	user            User
-	started         Time
-	description     string
-	job             Job
-	args            string
-	statistics      Statistics
-	ended           Time
-	abortedBy       User
-	successfulNodes []Node
-	failedNodes     []Node
-}
-
 const (
 	RUNNING = iota
 	SUCCEEDED
@@ -63,19 +32,60 @@ const (
 	ABORTED
 )
 
-type URL struct {
+type Execution struct {
+	uuid        uuid.UUID
+	output      url.URL
+	status      Status
+	user        User
+	started     time.Time
+	description string
+	job         Job
+	args        []string
+	statistics  Statistics
+	ended       time.Time
+	abortedBy   User
 }
 
-type User struct {
+type TimeSequenceEntry struct {
+	time  time.Time
+	value float64
+}
+
+type TimeSequence struct {
+	entries []TimeSequenceEntry
+}
+
+func (ts *TimeSequence) Max() (result float64) {
+	result = math.MinFloat64
+	for _, e := range ts.entries {
+		result = Fmax(result, e.value)
+	}
+}
+
+func (ts *TimeSequence) Min() (result float64) {
+	result = math.MaxFloat64
+	for _, e := range ts.entries {
+		result = Fmin(result, e.value)
+	}
+}
+
+func (ts *TimeSequence) Avg() (result float64) {
+	sum := 0.0
+	count := len(ts.entries)
+	for _, e := range ts.entries {
+		sum += e.value
+	}
+	result = sum / count
 }
 
 type Statistics struct {
+	EnvVar string
+	VmPeak TimeSequence // peak value of virtual memory
+	VmHWM  TimeSequence // peak value of VmRSS
+	VmSwap TimeSequence
 }
 
-type Time stuct {
-}
-
-type Node struct {
+type User struct {
 }
 
 //
